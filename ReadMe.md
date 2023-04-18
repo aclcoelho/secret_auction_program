fdb6599121c9a250236c9cf9f17d1a2e04e3a01d
aclcoelho <ana.lunardello@gmail.com>
2023-04-17 22:50:26 -0300

    Adding initial files

---
+++ ReadMe.md
@@ -0,0 +1,5 @@
+# Secret Auction Program
+
+### Each user can enter a bid in secret and, by writing "no" at the end, you figure out who's the winner with the largest bid.
+
+A part of the 100 Days of Python Challenge -  Day 9
---
+++ secret_auction_program.py
@@ -0,0 +1,31 @@
+import os
+
+logo = '''
+                         ___________
+                         \         /
+                          )_______(
+                          |"""""""|_.-._,.---------.,_.-._
+                          |       | | |               | | ''-.
+                          |       |_| |_             _| |_..-'
+                          |_______| '-' `'---------'` '-'
+                          )"""""""(
+                         /_________\\
+                       .-------------.
+                      /_______________\\
+'''
+
+print(logo)
+bids = {}
+answer = "yes"
+
+while answer == "yes":
+  name = input("What is your name?: ")
+  bid = int(input("What is your bid?: $"))
+  bids[name] = bid 
+  answer = (input("Are there any other bidders? Type 'yes' or 'no'. ")).lower()
+  os.system("clear")
+else:
+  print(bids)
+  max_bid = max(bids.values())
+  max_key = max(bids, key=bids.get)
+  print(f"The winner is {max_key} with a bid of ${max_bid}")
